c92007b1f317251026a8fb9c5225065300b70f54
Peter Stein <peterstein@protonmail.com>
2020-07-10 11:53:56 +0200

    Fix test_data.py: ValueError

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,10 +2,23 @@ import pytest
 
 from mao.constants import NUMBER_OF_KANGXI_RADICALS
 from mao.data import get_kangxi_radicals_table
-from tests.conftest import SKIP_DOWNLOAD_TESTS
+from mao.unihan import download_unihan_zip
+from tests.conftest import (
+    SKIP_DOWNLOAD_TESTS,
+    UNIHAN_TEST_PATH,
+    clear_unihan_test_files,
+)
+
+
+def setup_module():
+    download_unihan_zip(UNIHAN_TEST_PATH)
+
+
+def teardown_module():
+    clear_unihan_test_files()
 
 
 @pytest.mark.skipif(SKIP_DOWNLOAD_TESTS, reason="Skipping tests that require downloads")
 def test_get_kangxi_radicals_table():
-    dataframe = get_kangxi_radicals_table()
+    dataframe = get_kangxi_radicals_table(unihan_base_path=UNIHAN_TEST_PATH)
     assert len(dataframe) == NUMBER_OF_KANGXI_RADICALS
