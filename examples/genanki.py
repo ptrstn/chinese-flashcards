5a739a7cd4aca6d788f6719370f1eef6c7ea02c0
Peter Stein <peterstein@protonmail.com>
2020-07-30 21:22:05 +0200

    Create load_radicals_dataframe function

--- examples/genanki.py
+++ examples/genanki.py
@@ -3,47 +3,13 @@ import random
 import genanki
 import pandas
 
-from mao.hsk import read_all_hsk_files, add_hsk_level_column
-from mao.kangxi import retrieve_unicode_kangxi_table, add_is_kangxi_radical_column
-from mao.tidy import (
-    spread_unihan_dataframe_columns,
-    create_encoded_columns,
-    split_radical_additional_strokes_column,
-    clean_definition,
-)
-from mao.unihan import read_all_unihan_files
+from mao.data import load_radicals_dataframe
 
 pandas.set_option("display.max_rows", 500)
 pandas.set_option("display.max_columns", 500)
 pandas.set_option("display.width", 1000)
 
-dataframe = read_all_unihan_files()
-dataframe = spread_unihan_dataframe_columns(dataframe)
-dataframe = create_encoded_columns(dataframe)
-dataframe = split_radical_additional_strokes_column(dataframe)
-kangxi_table = retrieve_unicode_kangxi_table()
-dataframe = add_is_kangxi_radical_column(dataframe, kangxi_table=kangxi_table)
-hsk_table = read_all_hsk_files()
-dataframe = add_hsk_level_column(dataframe, hsk_table=hsk_table)
-df = dataframe[dataframe.is_kangxi_radical].copy()
-
-# https://stackoverflow.com/a/60024263/9907540
-df.loc[:, "strokes"] = df.kTotalStrokes.astype("float").astype("Int64")
-
-df.loc[:, "definition"] = df.apply(
-    lambda row: clean_definition(row.kDefinition)
-    if not pandas.isnull(row.kDefinition)
-    else row.kDefinition,
-    axis=1,
-)
-
-df = df[["glyph", "definition", "kMandarin", "variant_glyph", "radical", "strokes"]]
-
-df = df[df.strokes <= 4]
-
-df.sort_values(["strokes", "radical"], inplace=True)
-
-df.fillna("", inplace=True)
+radicals_df = load_radicals_dataframe()
 
 my_model = genanki.Model(
     random.randrange(1 << 30, 1 << 31),
@@ -65,7 +31,7 @@ my_model = genanki.Model(
 
 my_deck = genanki.Deck(random.randrange(1 << 30, 1 << 31), "Kangxi Radicals")
 
-for index, row in df.iterrows():
+for index, row in radicals_df.iterrows():
     my_note = genanki.Note(
         model=my_model, fields=[row.glyph, row.definition, row.variant_glyph]
     )
--- mao/data.py
+++ mao/data.py
@@ -1,8 +1,12 @@
-from mao.kangxi import retrieve_unicode_kangxi_table
+import pandas
+
+from mao.hsk import read_all_hsk_files, add_hsk_level_column
+from mao.kangxi import retrieve_unicode_kangxi_table, add_is_kangxi_radical_column
 from mao.tidy import (
     spread_unihan_dataframe_columns,
     create_encoded_columns,
     split_radical_additional_strokes_column,
+    clean_definition,
 )
 from mao.unihan import read_all_unihan_files, UNIHAN_BASE_PATH
 
@@ -37,3 +41,50 @@ def get_kangxi_radicals_table(unihan_base_path=UNIHAN_BASE_PATH):
     unified_table = radicals[radicals.glyph.isin(list(kangxi.unified_glyph))]
 
     return unified_table
+
+
+def load_radicals_dataframe():
+    dataframe = read_all_unihan_files()
+    dataframe = spread_unihan_dataframe_columns(dataframe)
+    dataframe = create_encoded_columns(dataframe)
+    dataframe = split_radical_additional_strokes_column(dataframe)
+    kangxi_table = retrieve_unicode_kangxi_table()
+    dataframe = add_is_kangxi_radical_column(dataframe, kangxi_table=kangxi_table)
+    hsk_table = read_all_hsk_files()
+    dataframe = add_hsk_level_column(dataframe, hsk_table=hsk_table)
+    radicals_df = dataframe[dataframe.is_kangxi_radical].copy()
+
+    # https://stackoverflow.com/a/60024263/9907540
+    radicals_df.loc[:, "strokes"] = radicals_df.kTotalStrokes.astype("float").astype(
+        "Int64"
+    )
+
+    radicals_df.loc[:, "definition"] = radicals_df.apply(
+        lambda row: clean_definition(row.kDefinition)
+        if not pandas.isnull(row.kDefinition)
+        else row.kDefinition,
+        axis=1,
+    )
+
+    radicals_df = radicals_df[
+        [
+            "glyph",
+            "definition",
+            "kMandarin",
+            "variant_glyph",
+            "simplified_glyph",
+            "traditional_glyph",
+            "radical",
+            "strokes",
+            "kFrequency",
+        ]
+    ]
+
+    radicals_df.kFrequency = radicals_df.kFrequency.fillna(value="9")
+    radicals_df[["kFrequency"]] = radicals_df[["kFrequency"]].fillna(value=9)
+    radicals_df.kFrequency = radicals_df.kFrequency.astype("int")
+
+    radicals_df.sort_values(["strokes", "kFrequency", "radical"], inplace=True)
+    radicals_df.fillna("", inplace=True)
+
+    return radicals_df
